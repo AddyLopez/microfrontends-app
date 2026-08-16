f1418b9257d43a00af3d6b41b92ac7eec89a7cfe
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-16 12:17:01 -0600

    Set up module federation plugin

--- ecomm/container/webpack.config.js
+++ ecomm/container/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // This plugin is to ensure that webpack bundle files (or script tags) are correctly inserted to public/index.html
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -6,6 +7,12 @@ module.exports = {
     port: 8080,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      new: "container",
+      remotes: {
+        products: "products@http://localhost:8081/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
