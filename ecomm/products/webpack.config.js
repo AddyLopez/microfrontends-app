90e83f742cac593411eab6a714680d1041a2be25
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-16 12:41:48 -0600

    Annotate purpose of remoteEntry.js

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // This plugin is to ensure that webpack bundle files (or script tags) are correctly inserted to public/index.html
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
+// remoteEntry.js lists different modules that can be loaded from the Products project
+
 module.exports = {
   mode: "development",
   devServer: {
