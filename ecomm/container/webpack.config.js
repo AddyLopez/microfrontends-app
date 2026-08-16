e66df731263c3265c0f39f1544032aced40d6614
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-16 00:56:07 -0600

    import plugin and configure webpack.config.js

---
+++ ecomm/container/webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // This plugin is to ensure that webpack bundle files (or script tags) are correctly inserted to public/index.html
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
