14f129e0f286548b25130f80ce7d31f74001fe3c
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-16 12:12:42 -0600

    Set up module federation plugin to expose modules
    The goal is to make Container the Host and Products the Remote module

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // This plugin is to ensure that webpack bundle files (or script tags) are correctly inserted to public/index.html
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -6,6 +7,13 @@ module.exports = {
     port: 8081,
   },
   plugins: [
+    new ModuleFederationPllugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
