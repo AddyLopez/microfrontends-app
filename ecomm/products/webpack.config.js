9d5c09a78192270d7614d089327c34cc72981abf
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-16 22:23:27 -0600

    Annotate code related to module federation plugin

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -1,7 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // This plugin is to ensure that webpack bundle files (or script tags) are correctly inserted to public/index.html
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin"); // This plugin enables set up for a remote modules to integrate with a host
 
 // remoteEntry.js lists different modules that can be loaded from the Products project
+// ProductsIndex is a descriptive alias
+// name property must match products@http://localhost:8081/remoteEntry.js in container/webpack.config.js
 
 module.exports = {
   mode: "development",
