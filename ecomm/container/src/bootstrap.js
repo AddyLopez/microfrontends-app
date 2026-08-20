56753e5ef8a744cb5c4715218dce5e1a4257fc10
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-08-19 22:55:08 -0600

    Import mount function from products to container

--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,4 +1,6 @@
-import "products/ProductsIndex";
+import { mount } from "products/ProductsIndex"; // imported from products/src/bootstrap.js
 import "cart/CartDisplay";
 
 console.log("container");
+
+mount(document.querySelector("#container-products"));
